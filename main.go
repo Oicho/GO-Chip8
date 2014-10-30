cfb4af59453e471652ff822c07a9d90c82e4e8db
Gauthier FRIDIERE <gauthier.fridiere@numergy.com>
2014-10-30 10:05:58 +0100

    Input checking done

--- main.go
+++ main.go
@@ -3,10 +3,13 @@
 package main
 
 import (
-	"github.com/nsf/termbox-go"
+	"strings"
 	"time"
+
+	"github.com/nsf/termbox-go"
 )
 
+// Memory represents the internal memory of the CHIP-8 emulator
 type Memory struct {
 	I          uint16
 	PC         uint16
@@ -21,22 +24,62 @@ type Memory struct {
 	Memory     [4096]byte
 }
 
+// Init must be called when right after you create a Memory variable
+// it initialize the screen array and set some values
 func (m *Memory) Init() {
 	m.PC = 0x200
 	m.Screen = make([][]bool, 64)
-	for i, _ := range m.Screen {
+	for i := range m.Screen {
 		m.Screen[i] = make([]bool, 32)
 	}
 }
 
+// CheckInputs verify if there is a key pressed
+// and then set the Key  array accordingly
 func (m *Memory) CheckInputs() {
+	for i := range m.Key {
+		m.Key[i] = false
+	}
 	const coldef = termbox.ColorDefault
-
 	ev := termbox.PollEvent()
 	if ev.Type == termbox.EventKey {
-		switch key := ev.Key; key {
-		case termbox.KeyCtrlA:
-			termbox.Close()
+		str := string(ev.Ch)
+		if str >= "A" {
+			str = strings.ToLower(str)
+		}
+		switch str {
+		case "1":
+			m.Key[0] = true
+		case "2":
+			m.Key[1] = true
+		case "3":
+			m.Key[2] = true
+		case "4":
+			m.Key[3] = true
+		case "q":
+			m.Key[4] = true
+		case "w":
+			m.Key[5] = true
+		case "e":
+			m.Key[6] = true
+		case "r":
+			m.Key[7] = true
+		case "a":
+			m.Key[8] = true
+		case "s":
+			m.Key[9] = true
+		case "d":
+			m.Key[10] = true
+		case "f":
+			m.Key[11] = true
+		case "z":
+			m.Key[12] = true
+		case "x":
+			m.Key[13] = true
+		case "c":
+			m.Key[14] = true
+		case "v":
+			m.Key[15] = true
 		}
 	}
 
@@ -59,10 +102,10 @@ func main() {
 	}
 	defer termbox.Close()
 
-	event_queue := make(chan termbox.Event)
+	eventQueue := make(chan termbox.Event)
 	go func() {
 		for {
-			event_queue <- termbox.PollEvent()
+			eventQueue <- termbox.PollEvent()
 		}
 	}()
 	termbox.Flush()
@@ -70,8 +113,12 @@ loop:
 	for {
 		termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
 		select {
-		case ev := <-event_queue:
-			tbprint(0, 10, coldef, coldef, string(ev.Ch))
+		case ev := <-eventQueue:
+			str := string(ev.Ch)
+			if str >= "A" {
+				str = strings.ToLower(str)
+			}
+			tbprint(0, 10, coldef, coldef, str)
 			termbox.Flush()
 
 			if ev.Type == termbox.EventKey && ev.Key == termbox.KeyEsc {
