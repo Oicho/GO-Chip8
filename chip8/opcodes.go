c36a804777d16b8e6e607c95cb9980c517e635dd
Gauthier FRIDIERE <gauthier.fridiere@numergy.com>
2014-11-07 17:48:35 +0100

    Adding opcode dispatch logic

--- chip8/opcodes.go
+++ chip8/opcodes.go
@@ -1,6 +1,14 @@
 package chip8
 
-import "math/rand"
+import (
+	"fmt"
+	"math/rand"
+	"reflect"
+)
+
+var eightFunctionArray = make([]func(*Memory, uint16), 8)
+var fFunctionMap = make(map[uint16]func(*Memory, uint16))
+var r = rand.New(rand.NewSource(99))
 
 // OneJumpTo is the 1NNN opcode which jump to the NNN address
 func OneJumpTo(m *Memory, opcode uint16) {
@@ -54,7 +62,16 @@ func SevenAddToRegister(m *Memory, opcode uint16) {
 	}
 }
 
-var r = rand.New(rand.NewSource(99))
+// EightArithmeticOperations is the dispatcher for 8XY? opcodes
+func EightArithmeticOperations(m *Memory, opcode uint16) {
+	code := opcode & 0x000F
+	if code == 0xE {
+		fmt.Println(reflect.TypeOf(EightArithmeticOperations))
+		// TODO == Do stuff
+	} else {
+		eightFunctionArray[code](m, opcode)
+	}
+}
 
 // ASetAddressRegister is the ANNN opcode
 // which set the Address register I to NNN
@@ -72,4 +89,9 @@ func BJumpToV0(m *Memory, opcode uint16) {
 // which set VX to a random number and NN
 func CSetToRandomNumber(m *Memory, opcode uint16) {
 	m.V[(opcode&0x0F00)>>16] = byte((opcode & 0x00FF)) & byte(r.Int63n(0x100))
+}
+
+// FUtils is the dispatcher for FNNN opcodes
+func FUtils(m *Memory, opcode uint16) {
+	fFunctionMap[opcode&0x00FF](m, opcode)
 }
